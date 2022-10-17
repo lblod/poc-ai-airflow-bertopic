c8e89c14d4b4c79ed1257adcc8930b784d7a800d
DelaetRuben <delaet.ruben@gmail.com>
2022-10-17 15:42:55 +0200

    Fixed passthrough filename mismatch

--- scripts/load.py
+++ scripts/load.py
@@ -23,7 +23,7 @@ def load(endpoint, query):
     # processing data from the response to the pandas data frame and saving it
     data = r.json()["results"]["bindings"]
     records = [{k: v['value'] for k, v in i.items()} for i in data]
-    write_json(file_name="export.json", content=records)
+    write_json(file_name="sparql_input.json", content=records)
 
 
 if __name__ == '__main__':
